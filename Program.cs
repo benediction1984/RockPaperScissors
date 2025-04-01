2df972fa4776e897973a0bca935ff2c386bf59af
John Paul Naranjo <clemency0916@yahoo.com>
2025-04-01 14:10:38 -0700

    Inital commit of RockPaperScissors

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Rock Paper Scissors
+string[] playerChoice = new[] { "rock", "paper", "scissors" };
+
+Console.WriteLine("Pick one of the selection: Rock, Paper, Scissors");
+string playerInput = Console.ReadLine().Trim().ToLower();
+
+
+Random rdm = new Random();
+string computerOpponentInput = playerChoice[rdm.Next(0, playerChoice.Length)];
+
+if (playerInput == computerOpponentInput)
+{
+    Console.WriteLine("It's a tie.");
+}
+else if (playerInput == "rock"     &&  computerOpponentInput == "scissors" ||
+         playerInput == "scissors" &&  computerOpponentInput == "paper" ||
+         playerInput == "paper"    &&  computerOpponentInput == "rock")
+{
+    Console.WriteLine($"You win! {playerInput.ToUpper()} beats {computerOpponentInput.ToUpper()}. Congratulations.");
+}
+else
+{
+    Console.WriteLine($"You lose! {computerOpponentInput.ToUpper()} beats {playerInput.ToUpper()}. Better luck next time. ");
+
+}
